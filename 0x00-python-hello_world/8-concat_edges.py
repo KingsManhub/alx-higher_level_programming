a6f9c5ab2f2299036d6318f8ead6c6e70f884792
Ayodele Stephen <ayodammys@gmail.com>
2022-08-23 02:37:17 +0100

    modified concat file

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
 language that combines remarkable power with very clear syntax"
-str = str[str.index("obj"):str.index("lang")] + str[str.index(" with"):str.index("ver")] + str[:6]
+str = str[39:66] + str[105:111] + str[:6]
 print(str)
