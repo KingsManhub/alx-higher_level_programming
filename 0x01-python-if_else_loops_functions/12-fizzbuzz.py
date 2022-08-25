acabd9ecab2e2b8cc64171375da3147cf9f073c0
Ayodele Stephen <ayodammys@gmail.com>
2022-08-25 21:56:19 +0100

    fizzbuzz file staged

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 
 def fizzbuzz():
-    for i in range(1, 101):
-        if i % 3 == 0:
-            print("Fizz", end=" ")
+    i = 1
+    while (i <= 100):
+        if ((i % 3 == 0) and (i % 5 == 0)):
+            print("FuzzBuzz", end=" ")
+        elif i % 3 == 0:
+            print("Fuzz", end=" ")
         elif i % 5 == 0:
             print("Buzz", end=" ")
-        elif i % 3 == 0 and i % 5 == 0:
-            print("FizzBuzz", end=" ")
-
+        else:
+            print("{}".format(i), end=" ")
+        i += 1
