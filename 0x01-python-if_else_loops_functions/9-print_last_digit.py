0cdd503cdd320c8574f68e8e48b792dedbf26ed6
Ayodele Stephen <ayodammys@gmail.com>
2022-08-25 14:14:11 +0100

    staging last digit file

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    if number < 0:
-        num = abs(number) % 10
-        num *= -1
-    else:
-        num = number % 10
+    num = number % 10
     return num
