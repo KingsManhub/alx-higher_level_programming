6f5a9ddd92700a760087fba7d1eee0b4fd8ff4ee
Ayodele Stephen <ayodammys@gmail.com>
2022-08-25 13:13:07 +0100

    modified uppercase file

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -5,9 +5,9 @@ def uppercase(str):
         if ord(i) >= 97 and ord(i) <= 122:
             i = chr(ord(i)-32)
         cap = ord(i) >= 65 and ord(i) <= 91
-        space = ord(i) == ord(" ") 
+        space = ord(i) == ord(" ")
         digit = ord(i) >= 48 and ord(i) <= 57
         if cap or space or digit:
             print("{}".format(i), end="")
         else:
-            print("")
+            print(" ")
