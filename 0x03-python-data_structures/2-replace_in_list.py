ac3bde0aeda869b788a11c71ca8348ebd4daca6e
Ayodele Stephen <ayodammys@gmail.com>
2022-08-29 00:56:13 +0100

    restaging modified replacing list file

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -6,8 +6,5 @@ def replace_in_list(my_list, idx, element):
     else:
         for i in range(len(my_list)):
             if i == idx:
-                new_list[i] = element
-            else:
-                new_list[i] = my_list[i]
-        my_list = new_list
-        return new_list, my_list
+                my_list[i] = element
+        return my_list
