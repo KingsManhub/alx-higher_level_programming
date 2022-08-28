aa943811f16d9a8459dfa6262101fd509569dfa1
Ayodele Stephen <ayodammys@gmail.com>
2022-08-28 22:55:01 +0100

    staging replacing list file

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def replace_in_list(my_list, idx, element):
+    if idx < 0 or idx > len(my_list):
+        return my_list
+    else:
+        for i in range(len(my_list)):
+            if i == idx:
+                new_list.append(element)
+            else:
+                new_list.append(my_list[i])
+        my_list = new_list
+        return new_list, my_list
