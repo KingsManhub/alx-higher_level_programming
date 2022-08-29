d55f23af89a3add12de718811ed98ce6d785754a
Ayodele Stephen <ayodammys@gmail.com>
2022-08-29 21:40:47 +0100

    7 add tuple file

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 def add_tuple(tuple_a=(), tuple_b=()):
-    tuple_a += (0, 0)
-    tuple_b += (0, 0)
-    return (tuple_a[0]+tuple_b[0], tuple_[1]+tuple_b[1])
+    res = []
+    for i in range(len(tuple_a)):
+        res.append(tuple_a[i] + tuple_b[i])
+    res = tuple(res)
+    return res
