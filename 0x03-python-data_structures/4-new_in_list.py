fb5cf93afdc2bdf4d25f97d0898ccd44ca653458
Ayodele Stephen <ayodammys@gmail.com>
2022-08-29 12:23:59 +0100

    staging new in list file

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 
 def new_in_list(my_list, idx, element):
+    new_list = []
     if (idx < 0) or (idx > len(my_list)):
         new_list = my_list
         return new_list
     else:
         for i in range(len(my_list)):
             if i == idx:
-                new_list[i] = element
+                new_list.insert(i, element)
             else:
-                new_list[i] = i
+                new_list.insert(i, my_list[i])
         return new_list
