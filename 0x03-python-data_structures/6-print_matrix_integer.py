a6598a9ef8f1fd27333967e33192ce95f889d698
Ayodele Stephen <ayodammys@gmail.com>
2022-08-29 13:47:29 +0100

    staging 6-matrix integer

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
-    if len(matrix) > 0:
-        for i in range(3):
-            for j in range(3):
-                print("{:3d}".format(matrix[i][j]), end="")
-            print("")
-    else:
-        print("")
-
+    for i in matrix:
+        for j in i:
+            print('{:2d}'.format(j))
