b95fa4ef30da4829a40e3143e32f3709f2524fba
Ayodele Stephen <ayodammys@gmail.com>
2022-08-30 01:01:20 +0100

    max integer file

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return None
+    else:
+        Max = 0
+        for i in my_list:
+            Max = i if i > Max else Max
+        return Max
