f39d4e8a55e4afeb90b4b0472372476a2f95a205
Ayodele Stephen <ayodammys@gmail.com>
2022-10-10 17:11:37 +0100

    staging the 0-add_integer file and it's test files

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Module of addition function"""
+
+def add_integer(a, b=98):
+    """Add two integer values together. 
+    If a float is encountered, it converts to an integer first before adding.
+    Args:
+        a(int): 'a'parameter of add_integer which will take the first argument
+        b=98(int): 'b'parameter with default value of 98, it'll take the second argument.
+    TypeError:
+        raises a TypeError when the args are not int or float
+    """
+    if  (type(a) in [int, float] and not (type(b) in [int, float])):
+        raise TypeError("b must be an integer")
+    elif (not (type(a) in [int, float]) and type(b) in [int, float]):
+        raise TypeError("a must be an integer")
+    elif not ((type(a) in [int, float]) and type(b) in [int, float]):
+        raise TypeError("a must be an integer")
+    return int(a)+int(b)
+
